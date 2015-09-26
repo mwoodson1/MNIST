110d7aae0eb744add3ebae24b98de3588d88e230
mwoodson1 <markus.woodson1@gmail.com>
2015-09-26 18:41:24 -0400

    blocks test

--- blocks/MNIST_blocks.py
+++ blocks/MNIST_blocks.py
@@ -3,32 +3,41 @@ x = tensor.matrix('features')
 
 from blocks.bricks.cost import CategoricalCrossEntropy
 
-print "everything ok"
+from blocks.bricks import Linear, Rectifier, Softmax, Tanh
 
-##############################################3
-from theano import tensor
-x = tensor.matrix('features')
-
-from blocks.bricks import Linear, Rectifier, Softmax
-
-input_to_hidden = Linear(name="input_to_hidden", input_dim=784, output_dim=100)
-h = Rectifier().apply(input_to_hidden.apply(x))
-hidden_to_output = Linear(name="hidden_to_output", input_dim=100, output_dim=10)
-y_hat = Softmax().apply(hidden_to_output.apply(h))
-
-y = tensor.lmatrix('targets')
 from blocks.bricks.cost import CategoricalCrossEntropy
-cost = CategoricalCrossEntropy().apply(y.flatten(), y_hat)
-
 from blocks.bricks import WEIGHT
 from blocks.graph import ComputationGraph
 from blocks.filter import VariableFilter
 
-cg = ComputationGraph(cost)
-W1, W2 = VariableFilter(roles=[WEIGHT])(cg.variables)
-cost = cost + 0.005 * (W1 ** 2).sum() + 0.005 * (W2 ** 2).sum()
-cost.name = 'cost_with_regularization'
+from blocks.bricks import MLP
+
+from blocks.initialization import IsotropicGaussian, Constant
 
 
 from fuel.datasets import MNIST
-mnist = MNIST(("train",))
+
+
+from fuel.streams import DataStream
+from fuel.schemes import SequentialScheme
+from fuel.transformers import Flatten
+
+
+from blocks.algorithms import GradientDescent, Scale
+
+mlp = MLP(activations=[Tanh(), Softmax()], dims=[784, 100, 10],
+		  weights_init=IsotropicGaussian(0.01), biases_init=Constant(0))
+mlp.initialize()
+
+x = tensor.matrix('features')
+y = tensor.matrix('targets')
+y_hat = mlp.apply(x)
+cost = CategoricalCrossEntropy().apply(y.flatten(), y_hat)
+error_state = MissclassificationRate().apply(y.flatten(), y_hat)
+
+mnist_train = MNIST(("train",))
+train_stream = Flatten(
+	DataStream.default_stream(
+		dataset=mnist_train,
+		iteration_scheme=SequentialScheme(mnist_train.num_examples, 128)),
+	which_sources=('features',))
